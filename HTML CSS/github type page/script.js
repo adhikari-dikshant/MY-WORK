124d33ac72e0a6fa3e41f55e72e094587f242b00
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2023-10-22 15:04:28 +0530

    trash

---
+++ HTML CSS/github type page/script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('avatar-navbar');
+const closeButton = document.querySelector('.close-button');
+const navigationMenu = document.querySelector('.navigation-menu');
+
+button.addEventListener('click', () => {
+    navigationMenu.classList.remove('none');
+    navigationMenu.classList.remove('hide');
+});
+
+closeButton.addEventListener('click', () => {
+    navigationMenu.classList.remove('hide');
+});
